bbaf24766151ba96ece3fa06fce18b10bef49077
Trishan Wagle <mailtotrishan@gmail.com>
2026-03-16 09:14:43 +0545

    feat: added types

---
+++ types/project.ts
@@ -0,0 +1,10 @@
+export type PageType = {
+  id: string;
+  name: string;
+  rootStyles: string;
+  htmlContent: string;
+  isLoading: boolean;
+  projectId?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
